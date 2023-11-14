617c8787843ccbc362df80d9e172d3e8162c806b
Ekaterina-Kaleda <144140578+Ekaterina-Kaleda@users.noreply.github.com>
2023-11-14 12:35:55 +0300

    Add files via upload

---
+++ WebJS1/JS.js
@@ -0,0 +1,57 @@
+// Задание *
+
+// let star = '';
+// for (let j = 1; j < 11; j++) {
+//     for (let i = 0; i < j; i++) {
+//         star += '*'; 
+//     }
+//     star += '\n';
+// }
+// alert(star);
+
+// console.log(star);
+
+// Задание 1
+
+// let nums = '';
+// for (let i = 5; i < 10; i++) {
+//     nums += i + ", ";
+// }
+// nums += 10;
+// console.log(nums);
+
+// Задание 2
+// nums = '';
+// for (let i = 20; i > 10; i--) {
+//     nums += i + ', ';
+// }
+// nums += 10;
+// console.log(nums);
+
+// Задание 3
+// nums = '';
+// for (let i = 2; i < 10; i+=2) {
+//     nums += i + ', ';
+// }
+// nums += 10;
+// console.log(nums);
+
+// Задание 4
+// let i = 2;
+// while(i <= 16) {   
+//     if (i % 2 === 0) {
+//         i++;
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+// Задание 5
+
+// let mass1 = [];
+// for (let i = 5; i < 11; i++) {
+//     mass1.push(i);
+// }
+// console.log(mass1);
